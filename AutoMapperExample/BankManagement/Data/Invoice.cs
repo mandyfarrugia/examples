8dacc49c6eb9710957a404728cf72b86c472af8c
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-03-29 18:38:05 +0200

    Defined a model class representing an invoice.

---
+++ AutoMapperExample/BankManagement/Data/Invoice.cs
@@ -0,0 +1,9 @@
+﻿namespace BankManagement.Data
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
