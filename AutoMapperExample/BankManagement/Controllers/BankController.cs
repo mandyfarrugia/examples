f0d032801a3f1f442b4a9ac392d9331c21deac8a
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2026-03-30 10:57:33 +0200

    Created project to practice the basics of AutoMapper.

---
+++ AutoMapperExample/BankManagement/Controllers/BankController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BankManagement.Data;
+using BankManagement.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankManagement.Controllers
+{
+    [Route("api/[controller]")]
+    public class BankController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public BankController(IMapper mapper)
+        {
+            this._mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetInvoice()
+        {
+            Invoice inMemoryInvoice = new Invoice()
+            {
+                Id = 1,
+                Description = "Grocery",
+                Amount = 100
+            };
+
+            //Send the DTO to the frontend/back to the user.
+
+            //Provide the instance of Invoice (from the database, for example) to be mapped to an instance of InvoiceDto.
+            InvoiceDto invoiceDto = this._mapper.Map<InvoiceDto>(inMemoryInvoice);
+
+            return Ok(invoiceDto);
+        }
+    }
+}
